7a1831eea892ec223daf86b9c07cb187d02d9857
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-08-19 16:59:29 +0200

    responsiveness

--- script.js
+++ script.js
@@ -135,4 +135,67 @@ $(document).ready(function () {
         animateElements.forEach(element => {
             element.classList.add("animate-visible");
         });
-    }); 
+
+
+     
+
+
+
+
+
+     $(".text1parag2:first, .text2parag2:first").addClass("text-animate");
+
+    // Define initial content for the divPart1 section
+    var initialText1 = "Free Microsoft 365 apps are easier to use in Edge";
+    var initialText2 = "Your web apps are just a click away. Get more done with built-in Microsft 465 features on Microsoft Edge.";
+
+    // Initialize the content of divPart1 with the initial text
+    $(".text1Part1").text(initialText1);
+    $(".text2Part1").text(initialText2);
+
+    $(".category-link").click(function (e) {
+        e.preventDefault();
+
+        var category = $(this).data("category");
+        var categoryTexts = {
+            sidebar: {
+                text1: "Free Microsoft 365 apps are easier to use in Edge",
+                text2: "Your web apps are just a click away. Get more done with built-in Microsft 465 features on Microsoft Edge."
+            },
+            create: {
+                text1: "Category 2",
+                text2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+            }
+        };
+
+        var newText1 = categoryTexts[category].text1;
+        var newText2 = categoryTexts[category].text2;
+
+        // Add animation classes and update the content of divPart1 based on the selected category
+        $(".text1Part1").addClass("animate-slide-out").one("animationend", function () {
+            $(this).removeClass("animate-slide-out");
+            $(this).text(newText1);
+            $(this).addClass("animate-slide-in");
+        });
+
+        $(".text2Part1").addClass("animate-slide-out").one("animationend", function () {
+            $(this).removeClass("animate-slide-out");
+            $(this).text(newText2);
+            $(this).addClass("animate-slide-in");
+        });
+
+        $(".text1Part1, .text2Part1, .tryNow1, .tryNow2").addClass("animate-slide-out").one("animationend", function () {
+            $(this).removeClass("animate-slide-out");
+            $(this).addClass("animate-slide-in");
+        });
+
+        $(".text1Part1").text(newText1);
+        $(".text2Part1").text(newText2);
+        $(".tryNow1").text("Try Now");
+        $(".tryNow2").text("Next");
+
+        // ... rest of your code ...
+    });
+
+
+}); 
