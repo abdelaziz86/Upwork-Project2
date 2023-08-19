e1241c9a02f7d6856131e5ac6543966000b5970f
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-08-19 14:32:06 +0200

    subcategories working fine

--- script.js
+++ script.js
@@ -38,18 +38,61 @@ $(document).ready(function () {
 
 
 
-         // Show the subcategories with fadeIn() and hide with fadeOut() when clicking on the First Category
+        $(".subcategory-link1").click(function (e) {
+            e.preventDefault();
+
+            var subcategory = $(this).data("subcategory");
+            var subcategoryTexts = {
+                electronics: "Product 1  subcategory 2",
+                mobile: "Product 2  subcategory 2",
+                computer: "Product 3  subcategory 2"
+            };
+            var subcategoryImage = {
+                electronics: "https://mk-media.mytek.tn/media/catalog/product/cache/4635b69058c0dccf0c8109f6ac6742cc/i/p/iphone-se-2022-64-go-midnight-apple.jpg",
+                mobile: "https://mk-media.mytek.tn/media/catalog/product/cache/4635b69058c0dccf0c8109f6ac6742cc/i/p/iphone_11_64_go_vert_-_apple.jpg",
+                computer: "https://mk-media.mytek.tn/media/catalog/product/cache/4635b69058c0dccf0c8109f6ac6742cc/i/p/iph-11-64-black_3.jpg"
+            };
+            var subcategoryDescriptions = {
+                electronics: "Product 1  subcategory 2 description",
+                mobile: "Product 2  subcategory 2 description",
+                computer: "Product 3  subcategory 2 description"
+            }
+
+            $("#image").fadeOut("slow", function () {
+                $(this).attr("src", subcategoryImage[subcategory]).fadeIn("slow");
+            });
+
+            $(".text1parag2, .text2parag2").removeClass("text-animate");
+
+            setTimeout(function () {
+                $(".text1parag2").text(subcategoryTexts[subcategory]).addClass("text-animate");
+                $(".text2parag2").text(subcategoryDescriptions[subcategory]).addClass("text-animate");
+            }, 500);
+        });
+
+
+
+
+
+
+
+
+
+         // CATEGORY 1 
         $(".category-link[data-category='sidebar']").click(function (e) {
             e.preventDefault();
             $(".subcategories2").hide();
             $(".subcategories1").fadeIn();
         });
 
-        // Show the SUB CATEGORIES 2 with fadeIn() and hide with fadeOut() when clicking on the Second Category
+        // CATEGORY 2
         $(".category-link[data-category='create']").click(function (e) {
             e.preventDefault();
             $(".subcategories1").hide();
             $(".subcategories2").fadeIn();
         });
+
+
+
 
     });
